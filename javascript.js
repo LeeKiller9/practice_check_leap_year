42d2ea82286ad6e63ce2d1fe824bb6983176d70d
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-12 11:31:03 +0700

    up index styles js file

---
+++ javascript.js
@@ -0,0 +1,38 @@
+function check_leap1() {
+    let year = parseInt(prompt("Enter a year"));
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                alert(year + " is a leap year");
+            } else {
+                alert(year + " is NOT a leap year");
+            }
+        } else {
+            alert(year + " is a leap year");
+        }
+    } else {
+        alert(year + " is NOT a leap year");
+    }
+}
+
+function check_leap2() {
+    let year = parseInt(prompt("Enter a year"));
+    let isLeapYear = false;
+    let isDivisibleBy4 = year % 4 === 0;
+    if (isDivisibleBy4) {
+        let isDivisibleBy100 = year % 100 === 0;
+        if (isDivisibleBy100) {
+            let isDivisibleBy400 = year % 400 === 0;
+            if (isDivisibleBy400) {
+                isLeapYear = true;
+            }
+        } else {
+            isLeapYear = true;
+        }
+    }
+    if (isLeapYear) {
+        alert(year + " is a leap year");
+    } else {
+        alert(year + " is NOT a leap year");
+    }
+}
